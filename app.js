70703815adcba65ee979512a99638fe23b90ef72
EC2 Default User <kazukipump7@gmail.com>
2022-08-30 07:29:15 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// alert('Hello World');alert('Hello World')
+// let hello = 'Hello World'
+// alert(hello);
+
+// let int1 = 1;
+// alert(int1);
+// 足し算
+// alert(4 + 3);
+// 引き算
+// alert(8 - 5);
+// 掛け算
+// alert(2 * 6);
+// 割り算
+// alert(10 / 2);
+// alert('Hello' + 'World');
